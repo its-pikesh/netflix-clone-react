b8908fce24ef376caa5a12a6d7632a19dcaebd96
its-pikesh <Pikesh.11606271@lpu.in>
2020-07-20 09:29:15 +0530

    Added Css

--- src/Row.js
+++ src/Row.js
@@ -1,25 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import axios from './axios'
-import requests from './requests';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}){
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(()=>{
-        //run once when the row load and don't run again
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results)
-            return request;
+  useEffect(() => {
+    //run once when the row load and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-        }
-        fetchData();
-    }, [fetchUrl]);
-    console.log(movies)
-    return(
-        <div>
-            <h2>{title}</h2>
-        </div>
-    )
+      <div className="row_posters">
+        {/*row_posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_PosterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Row;
